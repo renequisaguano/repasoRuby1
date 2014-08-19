3503e589ed4887e6af8915de289d194c6d130932
renequisaguano <renequisaguano@gmail.com>
2014-08-19 18:47:59 -0500

    agrenado operaciones con cadenas
    concatenadno cadenas

--- aplicacion1.rb
+++ aplicacion1.rb
@@ -8,15 +8,26 @@ class HolaMundo
     resta=2-3
     multiplicacion=2*3
     division=2/3
+    exponente=2**3
     puts suma
     puts resta
     puts multiplicacion
-    puts resta
+    puts division
+    puts exponente
+  end
+  
+  def cadenas()
+    resultado=2+3
+    cadena ="HOLA"
+    print "El resultado es: \"#{resultado}\" \n"
+    cadena << " MUNDO "
+    print cadena *3
   end
   
 end
 
 
 objeto=HolaMundo.new()
-objeto.saluda()
+#objeto.saluda()
+objeto.cadenas()
 gets()
