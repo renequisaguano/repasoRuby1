6ae2986038ec8d4bb0be3e225d9eaa8273a4be63
renequisaguano <renequisaguano@gmail.com>
2014-08-20 15:45:57 -0500

    Entrada de datos
    usando el metodo puts

---
+++ aplicacion6.rb
@@ -0,0 +1,16 @@
+class Datos
+  def inizialize()
+    
+  end
+  
+  def entradaDatos()
+    puts "Ingrese su nombre"
+    nombre = gets
+    print "hola "+nombre
+    
+  end
+end
+
+obj1=Datos.new()
+obj1.entradaDatos()
+
