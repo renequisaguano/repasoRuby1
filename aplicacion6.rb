22734f9139d2307f9de01e684d22a4d3dbc88520
renequisaguano <renequisaguano@gmail.com>
2014-08-20 15:55:33 -0500

    agregando arreglos

--- aplicacion6.rb
+++ aplicacion6.rb
@@ -9,8 +9,24 @@ class Datos
     print "hola "+nombre
     
   end
+  
+  def arreglos()
+    arreglo=["a","b"]<<"nuevo dato"
+    arreglo2=[]
+    puts [1,2,3][-1]
+    puts [1,20,30][0]
+    
+    for i in (0..3)
+      puts arreglo[i]
+    end
+    
+  end
 end
 
 obj1=Datos.new()
 obj1.entradaDatos()
+puts "Arreglos"
+
+obj1.arreglos()
+
 
