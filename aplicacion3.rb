93341cafe16ba21214f65324916f86d88eece865
renequisaguano <renequisaguano@gmail.com>
2014-08-19 20:50:57 -0500

    ciclo for

---
+++ aplicacion3.rb
@@ -0,0 +1,62 @@
+class Repetitivas
+  def initialize()
+
+  end
+
+  def cicloFor1()
+
+    for i in (1..10)
+      puts i
+
+    end
+
+  end
+
+  def cicloFor2()
+
+    for i in (1..10)
+      if i==4
+      break
+      end
+      puts i
+    end
+
+  end
+
+  def cicloFor3()
+
+    for i in (1..10)
+      if i==4
+      next
+      end
+      puts i
+    end
+
+  end
+    
+  def cicloFor4()
+
+    for i in (1..10)
+      if i==4
+      redo   #extra;o
+      end
+      puts i
+
+    end
+
+  end
+  
+  def pares()
+    for i in(1..20)
+      
+      if not i%2==0
+        next
+      end
+      puts i
+    end
+  end
+  
+end
+
+obj1=Repetitivas.new()
+obj1.pares
